93946dc4395441d8d4c74371bbd4e4a8fde2171b
Esra Akay <esra.akay67@icloud.com>
2024-08-04 02:22:45 +0200

    [Cosmetic] Reorganize code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const app = express();
-const path = require("path");
 const PORT = 3000; 
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
