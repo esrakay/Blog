346a0d853eaa44197403e508da38e11731accc35
Esra Akay <esra.akay67@icloud.com>
2024-08-04 02:40:08 +0200

    Sep up MongoDb

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const PORT = 3000;
 
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/blog")
+    .then(()=> {
+        console.log("MongoDB connected...");
+    })
+    .catch(err => {
+        console.error("Could not connect to MongoDB:", err);
+    })
 
 app.use(express.static(path.join(__dirname, "public")));
 
